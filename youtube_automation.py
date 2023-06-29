28662a26a01c949c94669ee38d809e5898ea53b1
Danilo Portela <daniloportela97@gmail.com>
2023-06-29 15:33:21 -0300

    Automatizar buscas no Youtube.

--- youtube_automation.py
+++ youtube_automation.py
@@ -6,7 +6,7 @@ from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://www.youtube.com/")
-driver.find_element(By.NAME, "search_query").send_keys("python")
+driver.find_element(By.NAME, "search_query").send_keys("Lud Session")
 driver.find_element(By.ID, "search-icon-legacy").click()
 
 print([my_href.get_attribute("href") for my_href in WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.ytd-video-renderer#video-title")))])
